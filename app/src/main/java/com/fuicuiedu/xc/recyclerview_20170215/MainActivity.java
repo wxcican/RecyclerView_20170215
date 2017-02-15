4bd4db0e0f61b944357dd6fbc58cc734be81e475
wxcican <wxcican@qq.com>
2017-02-15 17:25:48 +0800

    item点击事件的实现（接口回调）

--- app/src/main/java/com/fuicuiedu/xc/recyclerview_20170215/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/recyclerview_20170215/MainActivity.java
@@ -2,13 +2,16 @@ package com.fuicuiedu.xc.recyclerview_20170215;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private List<String> mData;
-    private MyAdapter adapter;
+    private MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,41 +48,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         //设置布局管理器
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.LayoutManager manager = new GridLayoutManager(this,3);
+        recyclerView.setLayoutManager(manager);
+
         //设置适配器
-        recyclerView.setAdapter(adapter = new MyAdapter());
-    }
-
-    //RecyclerView的适配器
-    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
-
-        //创建ViewHolder（泛型里，自己做的ViewHolder）
-        @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            MyViewHolder myViewHolder = new MyViewHolder(LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.item_view,parent,false));
-            return myViewHolder;
-        }
-
-        //数据绑定
-        @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            holder.textView.setText(mData.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return mData.size();
-        }
-
-        //自定义Viewholder
-        class MyViewHolder extends RecyclerView.ViewHolder{
-            TextView textView;
-
-            public MyViewHolder(View itemView) {
-                super(itemView);
-                textView = (TextView) itemView.findViewById(R.id.item_tv);
+        adapter = new MainAdapter(mData);
+        adapter.setOnItemClickListener(new MainAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int postion) {
+                Toast.makeText(MainActivity.this, "点击了" + postion, Toast.LENGTH_SHORT).show();
             }
-        }
+
+            @Override
+            public void onItemLongClick(View view, int postion) {
+                Toast.makeText(MainActivity.this, "长安了" + postion, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 }
